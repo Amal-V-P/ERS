0454a87f7eb9d8fcbf4c9b31b5adbf9965b13aef
Amal VP <amalvp121@gmail.com>
2026-03-22 14:21:14 +0530

    ERS backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -17,14 +17,10 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: 'favicon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'favicon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: 'vite.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml',
+            purpose: 'any'
           }
         ]
       }
